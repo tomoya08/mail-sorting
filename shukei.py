91a2e6552b5def70c58df7bd0c61d7ce711ba3db
tomoya08 <liverpoolfc_sg@icloud.com>
2020-10-13 15:37:35 +0900

    商品追加

--- shukei.py
+++ shukei.py
@@ -7,9 +7,20 @@ order = f.read()
 order_list = order.splitlines()
 
 #インデックスナンバーを取得
-red_index_number = order_list.index('※ 商品名 : オーガニックルイボスティー　ティーパック　2g×30包入り　　スーパーグレード （軽）')
-genmai_index_number = order_list.index('※ 商品名 : しょうが玄米ルイボスティー　ティーパック　2.5g×20包入り （軽）')
-index_number_list = [red_index_number, genmai_index_number]
+red_index_number = order_list.index('※ 商品名 : オーガニックルイボスティー\u3000ティーパック\u30002g×30包入り\u3000\u3000スーパーグレード （軽） ')
+orange_index_number = order_list.index('※ 商品名 : オレンジミックスルイボスティー\u3000ティーパック\u30002g×25包入り （軽） ')
+apricot_index_number = order_list.index('※ 商品名 : あんずルイボスティー　ティーパック　2g×25包入り （軽）')
+earlgray_index_number = order_list.index('※ 商品名 : アールグレイルイボスティー　ティーパック　2g×25包入り （軽）')
+muscat_index_number = order_list.index('※ 商品名 : マスカットルイボスティー　ティーパック　2g×25包入り （軽）')
+rose_index_number = order_list.index('※ 商品名 : ローズヒップルイボスティー　ティーパック　3g×20包入り （軽）')
+sg_index_number = order_list.index('※ 商品名 : しょうが玄米ルイボスティー\u3000ティーパック\u30002.5g×20包入り （軽） ')
+sp_index_number = order_list.index('※ 商品名 : スーパージンジャールイボスティー　ティーパック　2g×25包入り （軽）')
+mojito_index_number = order_list.index('※ 商品名 : モヒートルイボスティー　ティーパック　2g×25包入り （軽）')
+ainomi_index_number = order_list.index('※ 商品名 : 藍の実ルイボスティー　ティーパック　2.5g×20包入り （軽）')
+soltnut_index_number = order_list.index('※ 商品名 : マカデミアナッツ（薄塩）\u3000120g （軽） ')
+
+
+index_number_list = [red_index_number, orange_index_number, apricot_index_number, earlgray_index_number, muscat_index_number, rose_index_number, sg_index_number, sp_index_number, mojito_index_number, ainomi_index_number, soltnut_index_number]
 
 for index_number in index_number_list:
     #print(index_number)
@@ -23,16 +34,15 @@ for index_number in index_number_list:
     #print(kosuu)
 
     #レッドの商品名の乗っている行から商品名のみをプリント
-    print(name[8:21])
+    need_name = name[8:21]
+    print(need_name)
 
     #商品の個数が乗っている行から個数のみをプリント
-    print(kosuu[16:18])
-
+    need_kosuu = kosuu[16:18]
+    print(need_kosuu)
 
 f.close()
 
-
-
 #fo = open('/Users/mizumuratomoya/Desktop/pythontomo2/final.txt', 'w')
-#fo.write(str(red_name[8:21]))
+#fo.write(need_name)
 #fo.close()
